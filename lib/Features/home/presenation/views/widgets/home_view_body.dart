de96c70737b13db4ddd5db7d88ee81a5a1a6988d
Amira shawki <amirashawki7@gmail.com>
2024-10-24 22:50:55 +0300

    add style to text and add titleMedium to home

--- lib/Features/home/presenation/views/widgets/home_view_body.dart
+++ lib/Features/home/presenation/views/widgets/home_view_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly/Features/home/presenation/views/widgets/card_Book.dart';
 import 'package:bookly/Features/home/presenation/views/widgets/custom_appBar.dart';
 import 'package:bookly/Features/home/presenation/views/widgets/list_of_books.dart';
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/core/utils/styles.dart';
 
 import 'package:flutter/material.dart';
 
@@ -21,7 +22,13 @@ class HomeViewBody extends StatelessWidget {
               height: 20,
             ),
             listOfBooks(),
-            const Text('Best Saller')
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              'Best Saller',
+              style: Styles.titleMedium,
+            )
           ],
         ),
       ),
