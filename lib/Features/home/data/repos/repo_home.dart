49db3373366170a2d0544f9fb6bf509de1f6ae94
Amira shawki <amirashawki7@gmail.com>
2024-11-01 02:04:26 +0200

    create repo home methods and failures file

--- lib/Features/home/data/repos/repo_home.dart
+++ lib/Features/home/data/repos/repo_home.dart
@@ -1,5 +1,10 @@
-//repo create to    ا تحدد الهدف  م الفيتشر اي الا هيتعمل عندى   مش الهدف هيتعمل ازاى
+//RepoHome create to    ا تحدد الهدف  م الفيتشر اي الا هيتعمل عندى   مش الهدف هيتعمل ازاى
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+
+import 'package:bookly/core/utils/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class RepoHome {
-  fetchFeatureBooks();
-  fetchBestSellerBooks();
+  Future<Either<Failures, List<BookModel>>> fetchFeatureBooks();
+  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks();
 }
