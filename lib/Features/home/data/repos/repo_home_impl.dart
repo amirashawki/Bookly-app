6a8fdb4eda9fffbddb7bd3ee868bdefe98b6c93e
Amira shawki <amirashawki7@gmail.com>
2024-11-02 03:19:47 +0200

    implement fetch Feature Books methods

--- lib/Features/home/data/repos/repo_home_impl.dart
+++ lib/Features/home/data/repos/repo_home_impl.dart
@@ -29,7 +29,23 @@ class RepoHomeImpl implements RepoHome {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() async{
+
+      try {
+      var data = await apiServices.get(
+          endPoint:
+              'volumes?q=subject:programming&Filtering=free-ebooks');
+      List<BookModel> Books = [];
+      for (var item in data['items']) {
+        Books.add(item);
+      }
+      return right(Books);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return (left(ServerFailure.fromDioException(e)));
+      }
+      return (left(ServerFailure(e.toString())));
+    }
+   
   }
 }
